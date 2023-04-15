c516c2debe6439af8d06630569498559b13e34b8
nico <nicosis@gmail.com>
2023-04-15 15:37:42 +0200

    select vacio fixed

--- src/component/PokemonList.jsx
+++ src/component/PokemonList.jsx
@@ -10,11 +10,7 @@ const PokemonList = () => {
   const [currentPokeImg, setCurrentPokeImg] = useState("");
   const [currentPokeImgSh, setCurrentPokeImgSh] = useState("");
   const [shiny, setShiny] = useState(false);
-  console.log(currentPokeImgSh);
-  console.log(currentPokeImg);
-  // console.log(
-  //   currentPokeApi["sprites"].other["official-artwork"].front_default
-  // );
+  console.log(currentPokeName);
 
   useEffect(() => {
     fetch(pokeUrl + "?limit=30")
@@ -46,64 +42,73 @@ const PokemonList = () => {
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
+        <div className="container d-flex justify-content-center">
           <a className="navbar-brand" href="#">
-            <img src={logo} alt="Logo" height="60" />
+            <img src={logo} alt="Logo" height="50" />
           </a>
+
+          <select
+            className="form-select form-select text-center"
+            style={{ maxWidth: "360px" }}
+            aria-label=".form-select-lg example"
+            onChange={(e) => {
+              setCurrentPokeName(e.target.value);
+              setCurrentPokeUrl(
+                e.target.value === ""
+                  ? ""
+                  : pokesApi.find((poke) => poke.name === e.target.value).url
+              );
+            }}
+          >
+            <option value="" defaultValue>
+            Lista de Pokemones Disponibles
+            </option>
+            {pokesApi.map((itm, idx) => (
+              <option key={idx} value={itm.name}>
+                {itm.name.toUpperCase()}
+              </option>
+            ))}
+          </select>
         </div>
       </nav>
 
-      <select
-        className="form-select form-select mb-3 mx-auto mt-2"
-        style={{ maxWidth: "540px" }}
-        aria-label=".form-select-lg example"
-        onChange={(e) => {
-          setCurrentPokeName(e.target.value);
-          setCurrentPokeUrl(
-            pokesApi.find((poke) => poke.name === e.target.value).url
-          ); //no entiendo
-        }}
-      >
-        <option value="" defaultValue>
-          Selecciona tu Pokemon
-        </option>
-        {pokesApi.map((itm, idx) => (
-          <option key={idx} value={itm.name}>
-            {itm.name.toUpperCase()}
-          </option>
-        ))}
-      </select>
-
-      <div className="card mb-3 mx-auto" style={{ maxWidth: "540px" }}>
-        <div className="row g-0">
-          <div className="col-md-7">
-            <img
-              src={shiny ? currentPokeImg : currentPokeImgSh}
-              className="img-fluid rounded-start"
-              alt={currentPokeName + " img"}
-            />
-          </div>
-          <div className="col-md-5">
-            <div className="card-body">
-              <h3 className="card-title">{currentPokeName.toUpperCase()}</h3>
-              <p className="card-text fw-bold mb-1">
-                Experiencia base: {currentPokeApi.weight}
-              </p>
-              <p className="card-text">
-                Corresponde a la cantidad mínima de experiencia que recibe un
-                Pokémon al derrotar a otro.
-              </p>
-              <a
-                href="#"
-                class={shiny ? "btn btn-warning" : "btn btn-outline-warning"}
-                onClick={handleClickShiny}
-              >
-                Shiny
-              </a>
+      {currentPokeName === "" ? (
+        <div class="p-3 my-5 bg-danger text-white text-center">
+            <img src={logo} alt="Logo" />
+            <h2>Encuentra tu Pokemon Favorito</h2>
+            </div>
+      ) : (
+        <div className="card my-3 mx-auto" style={{ maxWidth: "600px" }}>
+          <div className="row g-0">
+            <div className="col-md-7">
+              <img
+                src={shiny ? currentPokeImg : currentPokeImgSh}
+                className="img-fluid rounded-start"
+                alt={currentPokeName + " img"}
+              />
+            </div>
+            <div className="col-md-5">
+              <div className="card-body">
+                <h2 className="card-title">{currentPokeName.toUpperCase()}</h2>
+                <h5 className="card-text fw-bold mb-1">
+                  Experiencia base: {currentPokeApi.weight}
+                </h5>
+                <p className="card-text">
+                  Corresponde a la cantidad mínima de experiencia que recibe un
+                  Pokémon al derrotar a otro.
+                </p>
+                <a
+                  href="#"
+                  class={shiny ? "btn btn-warning" : "btn btn-outline-warning"}
+                  onClick={handleClickShiny}
+                >
+                  Shiny
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
